3ea0006cf17c23f5dc555be9f69156e1fbbbe8d3
amckinney-1 <amckinney@student.neumont.edu>
2021-10-26 12:47:12 -0600

    Ray Tracing Sphere

---
+++ RayTracer/Geometry.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Types.h"
+#include "Ray.h"
+
+
+
+class Geometry
+{
+public:
+    Geometry() = default;
+    virtual ~Geometry() = default;
+
+    virtual bool Hit(const ray_t& r, float tMin, float tMax, raycastHit_t& hit) = 0;
+};
